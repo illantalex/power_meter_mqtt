5a0db28cd59f541b96e920d95905ffbeb0c8e275
Illya Antonuk <illantalex84@gmail.com>
2020-11-27 11:08:36 +0200

    added a script to install dependencies in Windows

---
+++ install_dep.bat
@@ -0,0 +1,6 @@
+mkdir lib\
+cd lib\
+git clone https://github.com/mandulaj/PZEM-004T-v30.git
+git clone https://github.com/UIPEthernet/UIPEthernet.git
+git clone https://github.com/bblanchon/ArduinoJson.git
+git clone https://github.com/knolleary/pubsubclient.git
